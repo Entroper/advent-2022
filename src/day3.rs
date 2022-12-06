40f0b40e1954406a8a826a78afbe346224d9e0f9
Entroper <entroper@gmail.com>
2022-12-06 06:03:38 -0500

    day3

---
+++ src/day3.rs
@@ -0,0 +1,75 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+pub fn part1() -> Result<(), Box<dyn Error>> {
+	let file = File::open("input03.txt")?;
+	let reader = BufReader::new(file);
+
+	let mut total = 0;
+	for line in reader.lines() {
+		let line = line?;
+		let half_length = line.len()/2;
+		let sack1 = &line[..half_length];
+		let sack2 = &line[half_length..];
+		total += get_matching_priority(sack1, sack2);
+	}
+
+	println!("{total}");
+
+	Ok(())
+}
+
+pub fn part2() -> Result<(), Box<dyn Error>> {
+	let file = File::open("input03.txt")?;
+	let reader = BufReader::new(file);
+
+	let mut total = 0;
+	let mut lines = reader.lines();
+	while let Some(line1) = lines.next() {
+		let line1 = line1?;
+		let line2 = lines.next().expect("Didn't read 3 lines")?;
+		let line3 = lines.next().expect("Didn't read 3 lines")?;
+		total += get_triple_priority(&line1, &line2, &line3);
+	}
+
+	println!("{total}");
+
+	Ok(())
+}
+
+fn get_matching_priority(sack1: &str, sack2: &str) -> i32 {
+	for char1 in sack1.chars() {
+		for char2 in sack2.chars() {
+			if char1 == char2 {
+				return match char1 {
+					'a'..='z' => char1 as i32 - 'a' as i32 + 1,
+					'A'..='Z' => char1 as i32 - 'A' as i32 + 27,
+					_ => 0
+				}
+			}
+		}
+	}
+
+	0
+}
+
+fn get_triple_priority(sack1: &str, sack2: &str, sack3: &str) -> i32 {
+	for char1 in sack1.chars() {
+		for char2 in sack2.chars() {
+			if char1 == char2 {
+				for char3 in sack3.chars() {
+					if (char1 == char3) {
+						return match char1 {
+							'a'..='z' => char1 as i32 - 'a' as i32 + 1,
+							'A'..='Z' => char1 as i32 - 'A' as i32 + 27,
+							_ => 0
+						}
+					}
+				}
+			}
+		}
+	}
+
+	0
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 
 mod day1;
 mod day2;
+mod day3;
 
 fn main() -> Result<(), Box<dyn Error>> {
 	day1::part1()?;
@@ -9,6 +10,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 	day2::part1()?;
 	day2::part2()?;
+
+	day3::part1()?;
+	day3::part2()?;
 
     Ok(())
 }
